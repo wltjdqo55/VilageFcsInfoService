4f9a8d06bf05d3d767302891304146ee476c47b9
지성배 <seongbae@eu-cast.com>
2024-10-14 13:16:36 +0900

    first

---
+++ src/main/java/fcs/web/vilage/VilageApplication.java
@@ -0,0 +1,13 @@
+package fcs.web.vilage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VilageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fcs/web/vilage/VilageApplicationTests.java
@@ -0,0 +1,13 @@
+package fcs.web.vilage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
